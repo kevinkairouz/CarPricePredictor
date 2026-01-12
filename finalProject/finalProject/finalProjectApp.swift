65880ad4786635001561bbddb8c9b1fc854e41ae
kevinkairouz <KM.Kairouz@gmail.com>
2026-01-12 18:34:00 -0500

    created codebase

---
+++ finalProject/finalProject/finalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectApp.swift
+//  finalProject
+//
+//  Created by Kev on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
