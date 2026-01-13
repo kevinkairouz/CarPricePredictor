709eb3a9526089cca0a64f25c441c90b0495d843
kevinkairouz <KM.Kairouz@gmail.com>
2026-01-12 22:42:31 -0500

    started Sign-In Screen

---
+++ finalProject/finalProject/SignIn.swift
@@ -0,0 +1,105 @@
+//
+//  ContentView.swift
+//  finalProject
+//
+//  Created by Kev on 1/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var color1: Color = .green
+    @State var color2: Color = .gray
+    
+    @State var emailSubmitted: String = ""
+    @State var passwordSubmitted: String = ""
+    
+    var body: some View {
+        
+        NavigationStack{
+            ZStack{
+                LinearGradient(colors: [color1, color2], startPoint: .top, endPoint: .bottomTrailing).ignoresSafeArea().animation(.easeInOut.speed(0.25).repeatForever(autoreverses: true), value: color1)
+                
+                VStack{
+                    Text("Welcome Back")
+                        .font(.system(size: 33, weight: .semibold, design: .serif))
+                    Spacer()
+                }.padding(.top, 135)
+                
+                
+                VStack(spacing: 30){
+                    TextField("Email", text: $emailSubmitted)
+                        .padding()
+                        .background(.gray.opacity(0.8))
+                        .cornerRadius(12)
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .padding()
+                        .shadow(radius: 8)
+                        .autocorrectionDisabled()
+                        .autocapitalization(.none)
+                    
+                    SecureField("Password", text: $passwordSubmitted)
+                        .padding()
+                        .background(.gray.opacity(0.8))
+                        .cornerRadius(12)
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .padding()
+                        .shadow(radius: 8)
+                        .autocorrectionDisabled()
+                        .autocapitalization(.none)
+                }
+                
+                ZStack{
+                    
+                    
+                    
+                    VStack{
+                        Spacer()
+                        Button {
+                            
+                        } label: {
+                            Text("Sign-In")
+                        }
+                        .frame(width: 330, height: 60)
+                        .background(.green)
+                        .cornerRadius(12)
+                        .foregroundStyle(.black)
+                        .fontWeight(.black)
+                        
+                    }.padding(.bottom, 200)
+                    
+                    VStack{
+                        Spacer()
+                        Image(systemName: "airplane")
+                            //MARK: make airplane bigger and when the button is tapped, make sure that the airplane gets bigger and flies to the end of the sign-in button and then takes you to home page t
+                        
+                    }.padding(.bottom, 220).offset(x: -145)
+                    
+                }
+                
+
+                
+
+                
+
+                
+
+                
+                
+            }
+        }
+       
+        
+        .onAppear {
+            color1 = .gray
+            color2 = .green
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
