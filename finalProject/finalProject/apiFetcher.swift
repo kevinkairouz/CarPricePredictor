9da2244c052f485de9766117861716795c84ba13
kevinkairouz <KM.Kairouz@gmail.com>
2026-01-15 18:08:34 -0500

    implemented api call method

--- finalProject/finalProject/apiFetcher.swift
+++ finalProject/finalProject/apiFetcher.swift
@@ -5,8 +5,11 @@
 //  Created by Kev on 1/13/26.
 //
 
+//MARK: may include api call to predict the car the person has as a bonus method 
+
 import Foundation
 import Combine
+
 
 struct CarInfo: Codable{
     let Brand: String
@@ -14,19 +17,19 @@ struct CarInfo: Codable{
     let Miles: Int
 
 }
-
 struct Result: Codable{
     let Price: Float
 }
-
-
 final class apiFetcher: ObservableObject{
-    
-    func fetchData(X: CarInfo) async throws -> Result {
-        return Result(Price: 1)
+        
+    func fetchData(X: CarInfo) async throws -> Float {
+        
+        let url = URL(string: "http://127.0.0.1:5000/predict")!
+        let (data, response) = try await URLSession.shared.data(from: url)
+        let decoder = JSONDecoder()
+        let res = try decoder.decode(Result.self, from: data)
+        return res.Price
+        
     }
-    
-    
-    
     
 }
