6d1fab3860594c83ebe1fa6a42f563912276861b
kevinkairouz <KM.Kairouz@gmail.com>
2026-01-15 13:53:57 -0500

    finished ML model & creating api route

--- finalProject/ML_model/app.py
+++ finalProject/ML_model/app.py
@@ -1,2 +1,21 @@
-from flask import Flask, jsonify, request 
+from flask import Flask, jsonify, request
 import model 
+
+app = Flask(__name__) 
+CodeyCopy = model.Codey()
+
+@app.route("/predict", methods = ["POST"]) 
+def makePrediction(user_X):  
+    if request.method == "POST":  
+        prediction_for_user = CodeyCopy.predict(user_X)  
+        response = {"price": prediction_for_user} 
+        return jsonify(response)
+        
+    else: 
+        return "Problem with this route" 
+    
+    
+def run():  
+    app.run() 
+
+run() 
--- finalProject/ML_model/model.py
+++ finalProject/ML_model/model.py
@@ -1,41 +1,26 @@
 #core model
 import pandas as pd  
 from sklearn.ensemble import RandomForestRegressor 
-from sklearn.linear_model import LinearRegression 
 from sklearn.model_selection import train_test_split 
 from sklearn.metrics import recall_score 
+from sklearn.metrics import precision_score
 
 df = pd.read_csv("car_price_prediction_.csv")
 Y = df[["Price"]]
 X = df.drop(["Car ID", "Price"], axis=1) 
 X = pd.get_dummies(X, columns=["Brand", "Fuel Type", "Transmission", "Condition", "Model"]) 
 
-
-
 class Codey: 
 
     def __init__(self): 
-        self.modelA = LinearRegression()
-        self.modelB = RandomForestRegressor() 
-        
-    
-    
+        self.model = RandomForestRegressor() 
     def train(self): 
-        X_train, X_test, Y_train, Y_test = train_test_split(X,Y, train_size=0.8, random_state=42, stratify=Y)   
-        self.modelA.fit(X_train, Y_train) 
-        
-        
-        
-        
-
-    def predict(self): 
-
-        """ 
-        this function will use train as a helper function in order to train the model and take a param 
-        
-        
-        """
-        return None
+        X_train, X_test, Y_train, Y_test = train_test_split(X,Y, train_size=0.8, random_state=42)   
+        self.model.fit(X_train, Y_train) 
+    def predict(self, user_X):  
+        self.train() 
+        Y_predicted = self.model.predict(user_X) 
+        return Y_predicted 
         
 
         
